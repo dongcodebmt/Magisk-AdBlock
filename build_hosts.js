953c3d54861947d0e8b939948953b50c614b6d40
dongcodebmt <admin@dongdev.com>
2024-06-11 22:51:54 +0700

    fix: some bugs and code optimization

--- build_hosts.js
+++ build_hosts.js
@@ -1,25 +1,39 @@
 #!/usr/bin/env node
 const fs = require('fs').promises;
-const regexIp = new RegExp('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', 'i');
 const saveDir = './';
 const filename = 'hosts';
+let skipHosts = [];
 
 async function main() {
     const configObj = JSON.parse(await fs.readFile('config.json', 'utf8'));
+    if (configObj.skipHosts?.length > 0) {
+        skipHosts = configObj.skipHosts;
+    }
     let blacks = [];
     let whites = [];
     for (const url of configObj.blackUrls) {
         const data = await fetchData(url);
-        blacks = [...blacks, ...strToArray(data)];
+        blacks = [...blacks, ...await getListFromHosts(data, 'Block: ')];
     }
     for (const url of configObj.whiteUrls) {
         const data = await fetchData(url);
-        whites = [...whites, ...strToArray(data, false)];
+        whites = [...whites, ...await getListFromHosts(data, 'Allow: ')];
     }
-    blacks = blacks.filter((el) => !whites.find(x => el.endsWith(x)));
+
+    let body = '';
+    if (configObj.defaultHosts?.length > 0) {
+        body = configObj.defaultHosts.join('\n') + '\n'
+    }
+    for (const item of blacks) {
+        const exists = whites.find(x => x === item);
+        if (!exists) {
+            body += appendIP(item) + '\n';
+        }
+    }
+
     try {
         await fs.mkdir(saveDir, { recursive: true });
-        await fs.writeFile(saveDir + filename, blacks.join('\n'));
+        await fs.writeFile(saveDir + filename, body);
         console.log('Completed!');
     } catch (e) {
         console.log('Error:', e.stack);
@@ -35,44 +49,78 @@ async function fetchData(url) {
     }
 }
 
-function strToArray(str, trimIp = false) {
-    const array = str.split('\n');
-    const newArray = [];
-    for (const item of array) {
-        if (item.startsWith('#')) {
+async function getListFromHosts(hosts, log = '') {
+    const list = hosts.split('\n');
+    const result = [];
+    for (const item of list) {
+        let line = item.trim().toLowerCase();
+        if (isEmpty(line)) {
             continue;
         }
-        let hostname = item.split('#')[0].trim().toLowerCase();
-        if (hostname === null || hostname === '') {
+        if (line.startsWith('#')) {
             continue;
         }
-        if (trimIp && hasIP(hostname)) {
-            hostname = removeIP(hostname);
-        }
-        if (!trimIp && !hasIP(hostname)) {
-            hostname = appendIP(hostname);
-        }
-        if (hostname.endsWith('0.0.0.0') || hostname.endsWith('127.0.0.1')) {
+        const obj = getHostObj(line);
+        if (!obj.success) {
             continue;
         }
-        if (newArray.includes(hostname)) {
+        if (result.includes(obj.hostname)) {
             continue;
         }
-        newArray.push(hostname.replace(/^\./, ''));
+        if (!isEmpty(log)) {
+            console.log(log + obj.hostname);
+        }
+        result.push(obj.hostname);
     }
-    return newArray;
-}
-
-function hasIP(ipaddress) {
-    return regexIp.test(ipaddress) ? true : false;
-}
-
-function removeIP(str) {
-    return str.replace(regexIp, '').trim();
+    return result;
 }
 
 function appendIP(str) {
     return `0.0.0.0 ${str.trim()}`;
 }
+
+function getHostObj(str) {
+    str = cleanLine(str);
+    
+    let flag = false;
+    const arr = str.split(' ');
+    // 1 only hostname
+    // 2 ip and hostname
+    if (arr.length !== 1 && arr.length !== 2) {
+        flag = true;
+    }
+    if (!flag && skipHosts.find(x => str.endsWith(x))) {
+        flag = true;
+    }
+    if (flag) {
+        return { success: false, hostname: null };
+    }
+    const hostname = hostTrim(arr.at(arr.length === 1 ? 0 : 1));
+    return { success: true, hostname };
+}
+
+function hostTrim(str) {
+    return str.trim().replace(/^\.|\.$/g, '');
+}
+
+function cleanLine(str) {
+    // remove comment
+    let line = str.split('#')[0].trim();
+    // multiple spaces to single space
+    line = str.replace(/\s\s+/g, ' ');
+    return line;
+}
+
+function isEmpty(str) {
+    return (!str || str.length === 0);
+}
+
+const log = console.log;
+console.log = (...strings) => {
+    const now = (new Date()).toISOString().slice(0, 19).replace(/-/g, '/').replace('T', ' ');
+    const dateFormat = '[' + now + '] INFO: ' + strings.shift();
+    strings.unshift(dateFormat);
+    log(...strings);
+};
 
 main();
